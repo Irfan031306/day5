519ee21f5291b785750f41bc9bba822668f56107
Irfan031306 <erfanhsbc@gmail.com>
2024-08-30 09:36:05 +0100

    Create day5.py

---
+++ day5.py
@@ -0,0 +1,15 @@
+print("Marvel Movie Character Creator")
+print()
+print("Answer these questions and let's find out.")
+print()
+Question1 = input("Do you like 'hanging around'?: ")
+if Question1 == "No":
+  print("Then you're not Spider-man")
+  print()
+  Question2 = input("Do you have a 'gravelly' voice?: ")
+  if Question2 == "No":
+    print("Aww, then you're not Korg")
+    print()
+    Question3 = input("Do you often feel 'Marvelous'?: ")
+    if Question3 == "Yes":
+      print("Aha! You're Captain Marvel! Hi!")
